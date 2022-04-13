d88256bb349b53404ea768bca35ce95c41ca18b3
Mathieu Mercier <merciermathieu412@gmail.com>
2022-04-13 11:07:37 -0400

    Procedural gen demo 1

--- world.gd
+++ world.gd
@@ -1,14 +1,102 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var noise
+var map_size = Vector2(200, 100)
+var grass_cap = 0.5
+var road_caps = Vector2(0.3,0.05)
+var enviroment_caps = Vector3(0.4, 0.3, 0.04)
+var ytree
+var ybush
 
+export (PackedScene) var trees: PackedScene= preload("res://World/Tree.tscn")
+export (PackedScene) var bushs: PackedScene= preload("res://World/bush.tscn")
+export (PackedScene) var bats: PackedScene= preload("res://Enemies/bat.tscn")
+export (PackedScene) var slimes: PackedScene= preload("res://Enemies/slime.tscn")
+export (PackedScene) var squeleton_mage: PackedScene= preload("res://Enemies/Squeletton_mage/Squeleton_mage.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$idlemusic.play()
+	randomize()
+	noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.octaves = 1.5
+	noise.period = 12
+	ytree = get_node("YSort/tree")
+	ybush = get_node("YSort/bush")
+#	noise.persistence = 0.7
+	make_road_map()
+	make_grass_map()
+	make_enviroment_map()
+	make_enemie()
+		
+func make_grass_map():
+	for x in map_size.x:
+		for y in map_size.y:
+			var a = noise.get_noise_2d(x,y)
+			if a > grass_cap:
+				$dirtwall.set_cell(x,y,0)
+	for x in map_size.x:
+		$dirtwall.set_cell(x,0,0)
+	for y in map_size.y:
+		$dirtwall.set_cell(0,y,0)
+	for y in map_size.y:
+		$dirtwall.set_cell(0,map_size.y,0)
+	for x in map_size.x:
+		$dirtwall.set_cell(map_size.x,0,0)
+	$dirtwall.update_bitmask_region(Vector2(0.0, 0.0), Vector2(map_size.x, map_size.y))
+	
+func make_road_map():
+	for x in map_size.x:
+		for y in map_size.y:
+			var a = noise.get_noise_2d(x,y)
+			if a < road_caps.x and a > road_caps.y and x != 0 and y != 0 and x != map_size.x and y != map_size.y:
+				$road.set_cell(x,y,0)
+	$road.update_bitmask_region(Vector2(0.0, 0.0), Vector2(map_size.x, map_size.y))
+	
+func make_enviroment_map():
+	var rng = RandomNumberGenerator.new()
+	for z in range(4):
+		for x in map_size.x:
+			for y in map_size.y:
+				var a = noise.get_noise_2d(x,y)
+				if a < enviroment_caps.x and a > enviroment_caps.y or a < enviroment_caps.z:
+					var chance = randi() % 100
+					if chance < 2:
+					
+						var num = rng.randi_range(0,2)
+						if num == 0:
+							var tree = trees.instance()
+							get_tree().current_scene.add_child(tree)
+							tree.global_position = Vector2(x*32,y*32)
+						if num == 1:
+							var bush = bushs.instance()
+							get_tree().current_scene.add_child(bush)
+							bush.global_position = Vector2(x*32,y*32)
+				
+				
+
+func make_enemie():
+	var rng = RandomNumberGenerator.new()
+	for z in range(2):
+		for x in map_size.x:
+			for y in map_size.y:
+				var a = noise.get_noise_2d(x,y)
+				if a < enviroment_caps.x and a > enviroment_caps.y or a < enviroment_caps.z:
+					var chance = randi() % 100
+					if chance < 2:
+					
+						var num = rng.randi_range(0,2)
+						if num == 0:
+							var slime = slimes.instance()
+							get_tree().current_scene.add_child(slime)
+							slime.global_position = Vector2(x*32,y*32)
+						if num == 1:
+							var bat = bats.instance()
+							get_tree().current_scene.add_child(bat)
+							bat.global_position = Vector2(x*32,y*32)
+
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
